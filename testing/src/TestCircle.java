aad3e2c12046b4da7e6d4b430dfeef6c8ac5b812
nkhan01 <nkhan01@stuy.edu>
2021-09-30 14:06:19 -0400

    Added perimeter method

--- testing/src/TestCircle.java
+++ testing/src/TestCircle.java
@@ -32,6 +32,10 @@ class Circle {
         return Math.PI * radius * radius;
     }
 
+    double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
     void setRadius(double newRadius) {
         radius = newRadius;
     }

16850af68b7234236d429d9f7c3f20acd519a18a
nvhiii <nahi.khan83@bcmail.cuny.edu>
2021-10-03 14:30:02 -0400

    Added some more codelab assignments

---
+++ testing/src/Accumulator.java
@@ -0,0 +1,12 @@
+public class Accumulator {
+    
+    private int sum = 0;
+    
+    public int getSum() {
+        return this.sum = sum;
+    }
+    
+    public void add(int add) {
+        sum += add;
+    }
+}
---
+++ testing/src/GasTankUpdated.java
@@ -0,0 +1,29 @@
+public class GasTankUpdated {
+    
+    private double amount = 0;
+    
+    public void addGas(double x) {
+        amount += x;
+    }
+    
+    public void useGas(double y) {
+        if (amount >= 0) {
+            amount -= y;
+        } else {
+            amount = 0;
+        }
+    }
+    
+    public boolean isEmpty() {
+        if (amount < 0.1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public double getGasLevel() {
+        return this.amount = amount;
+    }
+    
+}
